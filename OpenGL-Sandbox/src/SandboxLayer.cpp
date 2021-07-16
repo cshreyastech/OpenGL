0f7766152a1bb3a623b0952b1d32477ceeb9b999
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2021-07-16 16:32:19 -0700

    First Batch Rendering

--- OpenGL-Sandbox/src/SandboxLayer.cpp
+++ OpenGL-Sandbox/src/SandboxLayer.cpp
@@ -1,9 +1,11 @@
 #include "SandboxLayer.h"
 
+
 using namespace GLCore;
 using namespace GLCore::Utils;
 
 SandboxLayer::SandboxLayer()
+	: m_CameraController(16.0f / 9.0f)
 {
 }
 
@@ -15,22 +17,76 @@ void SandboxLayer::OnAttach()
 {
 	EnableGLDebugging();
 
-	// Init here
+	m_Shader = std::unique_ptr<Shader>(Shader::FromGLSLTextFiles(
+		"assets/shaders/shader.glsl.vert",
+		"assets/shaders/shader.glsl.frag"
+	));
+
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+	float vertices[] = {
+		-1.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		-0.5f,  0.5f, 0.0f,
+		-1.5f,  0.5f, 0.0f,
+
+		 0.5f, -0.5f, 0.0f,
+		 1.5f, -0.5f, 0.0f,
+		 1.5f,  0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f
+	};
+
+	glCreateVertexArrays(1, &m_QuadVA);
+	glBindVertexArray(m_QuadVA);
+
+	glCreateBuffers(1, &m_QuadVB);
+	glBindBuffer(GL_ARRAY_BUFFER, m_QuadVB);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glEnableVertexArrayAttrib(m_QuadVB, 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+
+	uint32_t indices[] = {
+		0, 1, 2, 2, 3, 0,
+		4, 5, 6, 6, 7, 4
+	};
+
+	glCreateBuffers(1, &m_QuadIB);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_QuadIB);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 }
 
 void SandboxLayer::OnDetach()
 {
-	// Shutdown here
+	glDeleteVertexArrays(1, &m_QuadVA);
+	glDeleteBuffers(1, &m_QuadVB);
+	glDeleteBuffers(1, &m_QuadIB);
 }
 
 void SandboxLayer::OnEvent(Event& event)
 {
-	// Events here
+	m_CameraController.OnEvent(event);
+}
+
+static void SetUniformMat4(uint32_t shader, const char* name, const glm::mat4& matrix)
+{
+	int loc = glGetUniformLocation(shader, name);
+	glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(matrix));
 }
 
 void SandboxLayer::OnUpdate(Timestep ts)
 {
-	// Render here
+	m_CameraController.OnUpdate(ts);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(m_Shader->GetRendererID());
+
+	auto vp = m_CameraController.GetCamera().GetViewProjectionMatrix();
+	SetUniformMat4(m_Shader->GetRendererID(), "u_ViewProj", vp);
+
+	glBindVertexArray(m_QuadVA);
+	SetUniformMat4(m_Shader->GetRendererID(), "u_Transform", glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f)));
+	glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, nullptr);
 }
 
 void SandboxLayer::OnImGuiRender()
--- OpenGL-Sandbox/src/SandboxLayer.h
+++ OpenGL-Sandbox/src/SandboxLayer.h
@@ -2,6 +2,8 @@
 
 #include <GLCore.h>
 #include <GLCoreUtils.h>
+#include <memory>
+#include<glm/glm.hpp>
 
 class SandboxLayer : public GLCore::Layer
 {
@@ -15,4 +17,12 @@ public:
 	virtual void OnUpdate(GLCore::Timestep ts) override;
 	virtual void OnImGuiRender() override;
 private:
+	std::unique_ptr<GLCore::Utils::Shader> m_Shader;
+	GLuint m_QuadVA = 0, m_QuadVB = 0, m_QuadIB = 0;
+	GLCore::Utils::OrthographicCameraController m_CameraController;
+
+	glm::vec4 m_SquareColor = {1.0f, 1.0f, 1.0f, 1.0f };
+	GLint m_ChernoTex, m_HazelTex;
+
+	glm::vec2 m_QuadPosition = { -1.5f, -0.5f };
 };
