80f2a96057a123c84a367ad91751242d20840a32
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2021-07-29 07:34:40 -0700

    Implemened refraction to handle multiple shapes use batch rendering

--- OpenGL-Sandbox/src/LineHandler.cpp
+++ OpenGL-Sandbox/src/LineHandler.cpp
@@ -7,7 +7,6 @@ LineHandler::LineHandler(const std::string lineHandler, const int* indexSequence
 	const uint32_t indexOffset, const uint32_t vertexOffset, const size_t maxLineCount)
 	:ShapeHandler(lineHandler, indexSequence, type, indexOffset, vertexOffset, maxLineCount)
 {
-
 }
 
 LineHandler::~LineHandler()
@@ -17,29 +16,6 @@ LineHandler::~LineHandler()
 void LineHandler::DrawLine(const glm::vec3 positions[], const glm::vec4& color, const glm::vec2 TexIndices[])
 {
 	ShapeHandler::DrawShape(positions, color, TexIndices);
-	//if (indexCount >= MaxIndexCount)
-	//{
-	//	EndBatch();
-	//	Flush();
-	//	BeginBatch();
-	//}
-
-	//float textureIndex = 0.0f;
-
-	//for (int i = 0; i < VertexOffset; i++)
-	//{
-	//	quadBufferPtr->Position = positions[i];
-	//	quadBufferPtr->Color = color;
-	//	quadBufferPtr->TexCoords = TexIndices[i];
-	//	quadBufferPtr->TexIndex = textureIndex;
-	//	quadBufferPtr++;
-	//}
-
-	//indexCount += IndexOffset;
-
-	//RenderStats.QuadCount++;
-	//RenderStats.VertexCount = RenderStats.QuadCount * VertexOffset;
-	//RenderStats.IndexCount = RenderStats.QuadCount * IndexOffset;
 }
 
 void LineHandler::Flush()
